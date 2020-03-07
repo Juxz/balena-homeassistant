75241c8336cb38835332bc362da480f661ec04ee
Ju <julien.larios@gmail.com>
2020-03-07 23:15:52 +0100

    tested ok

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-# Installing HACS if missing from config folder
+# Installing HACS and PyLoxone custom_components before starting HA
 
 
-if [ ! -d "/config/custom_components/hacs" ]; then
-
+if [ ! -d "/config/custom_components" ]; then
   printf "### Creating custom_components folder"
-
   mkdir /config/custom_components
-
 fi
 
 
 if [ ! -d "/config/custom_components/hacs" ]; then
-
   printf "### HACS plugin not found. Installing latest release from GitHub repo"
   # Required if moving to balenaos base image
   # install_packages git
-
   git clone https://github.com/hacs/integration.git
-  mv integration/custom_components/hacs /config/hacs
+  mv integration/custom_components/hacs /config/custom_components/hacs
   rm -rf integration
-
 fi
 
 if [ ! -d "/config/custom_components/loxone" ]; then
-
   printf "### Loxone custom_components not found. Installing latest release from GitHub repo"
   # Required if moving to balenaos base image
   # install_packages git
-
   git clone https://github.com/JoDehli/PyLoxone.git
   mv PyLoxone/custom_components/loxone /config/custom_components
   mv PyLoxone/www /config
